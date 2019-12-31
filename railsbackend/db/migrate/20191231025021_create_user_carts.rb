b89ccbc5a7fbc9e6cb6871ee6e5e5a86c6683b26
jehough <teachlife@outlook.com>
2019-12-30 22:02:01 -0500

    establishes relationships between user, cart and items

--- railsbackend/db/migrate/20191231025021_create_user_carts.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserCarts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_carts do |t|
-
-      t.timestamps
-    end
-  end
-end
--- railsbackend/db/migrate/20191231025248_create_cart_items.rb
+++ railsbackend/db/migrate/20191231025248_create_cart_items.rb
@@ -1,7 +1,8 @@
 class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
-
+      belongs_to :cart, foreign_key: true
+      belongs_to :item, foreign_key: true
       t.timestamps
     end
   end
