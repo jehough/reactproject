a1215d51086a6323d328621110e288c9207ccdc5
jehough <teachlife@outlook.com>
2019-12-30 20:47:01 -0500

    adds jwt and bcrypt gems and encapsulation

--- railsbackend/db/migrate/20191231014120_create_users.rb
+++ railsbackend/db/migrate/20191231014120_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :email
       t.string :passwoord_digest
-
+      t.boolean :admin
       t.timestamps
     end
   end
