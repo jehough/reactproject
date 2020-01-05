a877b6c3cee8b299cc14699be914273d1b2b5ec4
jehough <teachlife@outlook.com>
2020-01-05 14:13:54 -0500

    initial setup

---
+++ jsfrontend/src/views/components/header.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+
+  render(){
+    return(<header>
+      <h1>This is the starting point</h1>
+    </header>)
+  }
+}
