6e9048895e3afe4efa39c971a3059211c429dd68
jehough <teachlife@outlook.com>
2020-01-05 16:43:54 -0500

    setting up bootstrap

--- jsfrontend/src/views/components/navbar.js
+++ jsfrontend/src/views/components/navbar.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Navbar extends Component {
 
   render(){
-    return(<nav>
+    return(<nav className="navbar navbar-inverse">
       <h1>a menu will go here</h1>
     </nav>)
   }
