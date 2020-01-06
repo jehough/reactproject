d4af1dac4c3ed326ca240dcb178329a49549d914
jehough <teachlife@outlook.com>
2020-01-06 08:27:48 -0500

    adds more seed data

--- railsbackend/db/seeds.rb
+++ railsbackend/db/seeds.rb
@@ -7,3 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 fish = Category.create(name: "Fish and Marine Life")
 equipment = Category.create(name: "Equipment")
+
+# Fish Categories
+tropical = Subcategory.create(name: "Tropical Fish", category: fish)
+african = Subcategory.create(name: "African Ciclids", category: fish)
+south = Subcategory.create(name: "South American Ciclids", category: fish)
+salt = Subcategory.create(name: "Saltwater", category: fish)
+coral = Subcategory.create(name: "Coral and Live Rock", category: fish)
+koi = Subcategory.create(name: "Koi", category: fish)
+
+# Equipment Categories
+aquarium = Subcategory.create(name: "Aquarium kits", category: equipment)
+tank = Subcategory.create(name: "Tanks", category: equipment)
+filter = Subcategory.create(name: "Filtration and Sumps", category: equipment)
+rock = Subcategory.create(name: "Gravel and Sand", category: equipment)
