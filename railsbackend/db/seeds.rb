7de1813bb5586794b5330155dd6b11454a30ac0d
jehough <teachlife@outlook.com>
2020-01-06 08:45:27 -0500

    finishes category and subcategory seeding

--- railsbackend/db/seeds.rb
+++ railsbackend/db/seeds.rb
@@ -15,11 +15,17 @@ south = Subcategory.create(name: "South American Ciclids", category: fish)
 salt = Subcategory.create(name: "Saltwater", category: fish)
 coral = Subcategory.create(name: "Coral and Live Rock", category: fish)
 koi = Subcategory.create(name: "Koi", category: fish)
-lplants = Subcategory.create(name: "Live Plants", category: fish)
+plants = Subcategory.create(name: "Live Plants", category: fish)
+salef = Subcategory.create(name: "Fish on Sale", category: fish)
 
 # Equipment Categories
 aquarium = Subcategory.create(name: "Aquarium kits", category: equipment)
 tank = Subcategory.create(name: "Tanks", category: equipment)
 filter = Subcategory.create(name: "Filtration and Sumps", category: equipment)
 rock = Subcategory.create(name: "Gravel and Sand", category: equipment)
-plants = Subcategory.create(name:)
+plants = Subcategory.create(name: "Aquarium Decoration", category: equipment)
+accessory = Subcategory.create(name: "Accessory", category: equipment)
+salee = Subcategory.create (name: "Equipment on Sale", category: equipment)
+
+
+#Initial Items
