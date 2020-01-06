bf783d8af0925b5acfccc34c6501a61ae46ada5a
jehough <teachlife@outlook.com>
2020-01-06 08:32:48 -0500

    adds categories to subcategories table

---
+++ railsbackend/db/migrate/20200106133146_add_categories_refto_subcategory.rb
@@ -0,0 +1,5 @@
+class AddCategoriesReftoSubcategory < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :categories, :subcategory, foreign_key: true
+  end
+end
--- railsbackend/db/seeds.rb
+++ railsbackend/db/seeds.rb
@@ -15,9 +15,11 @@ south = Subcategory.create(name: "South American Ciclids", category: fish)
 salt = Subcategory.create(name: "Saltwater", category: fish)
 coral = Subcategory.create(name: "Coral and Live Rock", category: fish)
 koi = Subcategory.create(name: "Koi", category: fish)
+lplants = Subcategory.create(name: "Live Plants", category: fish)
 
 # Equipment Categories
 aquarium = Subcategory.create(name: "Aquarium kits", category: equipment)
 tank = Subcategory.create(name: "Tanks", category: equipment)
 filter = Subcategory.create(name: "Filtration and Sumps", category: equipment)
 rock = Subcategory.create(name: "Gravel and Sand", category: equipment)
+plants = Subcategory.create(name:)
