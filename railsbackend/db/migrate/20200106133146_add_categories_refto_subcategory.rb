de70ff1dffa714d51856dc78f25b0880a22a84df
jehough <teachlife@outlook.com>
2020-01-06 08:39:51 -0500

    fixes issues with associations

--- railsbackend/db/migrate/20200106133146_add_categories_refto_subcategory.rb
+++ railsbackend/db/migrate/20200106133146_add_categories_refto_subcategory.rb
@@ -1,5 +1,5 @@
 class AddCategoriesReftoSubcategory < ActiveRecord::Migration[5.2]
   def change
-    add_reference :categories, :subcategory, foreign_key: true
+    add_reference :subcategories, :category, foreign_key: true
   end
 end
--- railsbackend/db/schema.rb
+++ railsbackend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_131232) do
+ActiveRecord::Schema.define(version: 2020_01_06_133146) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2020_01_06_131232) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
