193fa41eccbb85c0114a09f45d3220aeae7ac4a1
jehough <teachlife@outlook.com>
2020-01-07 17:31:50 -0500

    adjusts image size on table

--- jsfrontend/src/views/components/item_list.js
+++ jsfrontend/src/views/components/item_list.js
@@ -2,17 +2,16 @@ import React, {Component} from 'react';
 import LinkDisplay from './link.js'
 
 export default class ItemList extends Component {
-  createrow = (item) => {
-      return(
-        <tr>
-          <td> <img src={`http://localhost:4000${item.attributes.image_url}`} alt={`${item.id} image`}/></td>
-          <td><LinkDisplay object={item} path="items" /></td>
-        </tr>)}
+  createrow = (item) => (<tr>
+    <td> <img src={`http://localhost:4000${item.attributes.image_url}`} className="previewImg" alt={`${item.id} image`}/></td>
+    <td><LinkDisplay object={item} path="items" /></td>
+    <td>{item.attributes.price}</td>
+    </tr>)
   render(){
     return(<div>
       {console.log(this.props.list)}
       <table>
-        {this.props.list.map(item => {this.createrow(item)})}
+        {this.props.list.map(item => this.createrow(item))}
       </table>
 
     </div>)
