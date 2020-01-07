8ed9b6f4875bcc83b5053d0b6afb7b6eeba3cb6d
jehough <teachlife@outlook.com>
2020-01-07 16:19:44 -0500

    sets up table for itemlist

--- jsfrontend/src/views/components/item_list.js
+++ jsfrontend/src/views/components/item_list.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import LinkDisplay from './link.js'
 
 export default class ItemList extends Component {
-
+  createrow = (item) => {
+      return(
+        <tr>
+          <td> <img src={`http://localhost:4000${item.attributes.image_url}`} alt={`${item.id} image`}/></td>
+          <td><LinkDisplay object={item} path="items" /></td>
+        </tr>)}
   render(){
-
     return(<div>
+      {console.log(this.props.list)}
       <table>
-      {this.props.list.map(item => {
-        <tr key={item.id}>
-        <td> <img src={`http://localhost:4000${item.attributes.image_url}`} alt="item image"/></td>
-        <td><LinkDisplay object={item} path="items" /></td>
-        </tr>})}
+        {this.props.list.map(item => {this.createrow(item)})}
       </table>
 
-          </div>)
+    </div>)
   }
 }
