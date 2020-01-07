1b849f5b5ea0149c513add9c95df70053b25d0f1
jehough <teachlife@outlook.com>
2020-01-07 14:54:17 -0500

    updates item serializer with needed info

--- railsbackend/app/serializers/item_serializer.rb
+++ railsbackend/app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :price
+  attributes :name, :description, :price, :gender
 
   attribute :image_url do |item|
     Rails.application.routes.url_helpers.rails_blob_url(item.image, only_path: true)
