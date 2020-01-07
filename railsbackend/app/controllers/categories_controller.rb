07671058700c2e49a279faafff4d61c2b4c891bd
jehough <teachlife@outlook.com>
2020-01-06 20:32:35 -0500

    troubleshooting serializers

---
+++ railsbackend/app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+class CategoriesController < ApplicationController
+  skip_before_action :authenticate_request
+  def index
+    categories = Category.all
+    options = {include: [:subcategories]}
+    render json: CategorySerializer.new(categories)
+  end
+end
