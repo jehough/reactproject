64848b3fabe13b21714b1aabc526146a763c5496
jehough <teachlife@outlook.com>
2020-01-07 09:19:08 -0500

    establishes links for equipment and fish pages

--- jsfrontend/src/views/components/navbar.js
+++ jsfrontend/src/views/components/navbar.js
@@ -12,8 +12,8 @@ export default class Navigation extends Component {
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <NavDropdown title="Categories" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#fish">Fish</NavDropdown.Item>
-              <NavDropdown.Item href="#equipment">Equipment</NavDropdown.Item>
+              <NavDropdown.Item href="fish">Fish</NavDropdown.Item>
+              <NavDropdown.Item href="equipment">Equipment</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           </Navbar.Collapse>
--- jsfrontend/src/views/containers/categories.js
+++ jsfrontend/src/views/containers/categories.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
 
   render(){
     return(<div>
-      <h1>Category</h1>
+      <h1>Category: {this.props.division}</h1>
 
     </div>)
   }
