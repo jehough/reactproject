6780b28eebd8c4b75e6fd5460a590733726acd1d
jehough <teachlife@outlook.com>
2020-01-06 19:41:58 -0500

    completes seed data

--- railsbackend/db/seeds.rb
+++ railsbackend/db/seeds.rb
@@ -23,7 +23,7 @@ aquarium = Subcategory.create(name: "Aquarium kits", category: equipment)
 tank = Subcategory.create(name: "Tanks", category: equipment)
 filter = Subcategory.create(name: "Filtration and Sumps", category: equipment)
 rock = Subcategory.create(name: "Gravel and Sand", category: equipment)
-plants = Subcategory.create(name: "Aquarium Decoration", category: equipment)
+decor = Subcategory.create(name: "Aquarium Decoration", category: equipment)
 accessory = Subcategory.create(name: "Accessory", category: equipment)
 salee = Subcategory.create (name: "Equipment on Sale", category: equipment)
 
@@ -38,22 +38,52 @@ gourami.image.attach(
 community.items << gourami
 
 neon = Item.create(name: "Neon", quantity: 40, description: "Random stuff to get started", price: 2.99, gender: "N/A")
-gourami.image.attach(
+neon.image.attach(
   io: File.open('/home/jim/Pictures/FishImages/neon-tetra.jpg'),
   filename: 'neon-tetra.jpg',
 )
 community.items << neon
 
 peacock = Item.create(name: "Red Sun Peacock Cichlid", quantity: 10, description: "Random stuff to get started", price: 12.99, gender: "Male")
-gourami.image.attach(
+peacock.image.attach(
   io: File.open('/home/jim/Pictures/FishImages/peacock.jpeg'),
   filename: 'peacock.jpeg',
 )
 african.items << peacock
 
 frontosa = Item.create(name: "Frontosa Cichlid", quantity: 10, description: "Random stuff to get started", price: 12.99, gender: "Female")
-gourami.image.attach(
+frontosa.image.attach(
   io: File.open('/home/jim/Pictures/FishImages/frontosa.jpeg'),
   filename: 'frontosa.jpeg',
 )
 african.items << frontosa
+
+jack = Item.create(name: "Jack Dempsy Cichlid", quantity: 10, description: "Random stuff to get started", price: 12.99, gender: "Female")
+jack.image.attach(
+  io: File.open('/home/jim/Pictures/FishImages/jackDempsy.jpeg'),
+  filename: 'jackDempsy.jpeg',
+)
+south.items << jack
+
+ram = Item.create(name: "Ram Cichlid", quantity: 10, description: "Random stuff to get started", price: 12.99, gender: "Female")
+ram.image.attach(
+  io: File.open('/home/jim/Pictures/FishImages/ram.jpeg'),
+  filename: 'ram.jpeg',
+)
+south.items << ram
+
+sale.items << jack
+
+tengal = Item.create(name: "Ten Gallon Kit", quantity: 15, description: "Random stuff to get started", price: 25.99, gender: "N/A")
+tengal.image.attach(
+  io: File.open('/home/jim/Pictures/FishImages/tengal.jpeg'),
+  filename: 'tengal.jpeg',
+)
+aquarium.items << tengal
+
+twengal = Item.create(name: "Twenty Gallon Kit", quantity: 15, description: "Random stuff to get started", price: 35.99, gender: "N/A")
+twengal.image.attach(
+  io: File.open('/home/jim/Pictures/FishImages/twengal.jpeg'),
+  filename: 'twengal.jpeg',
+)
+aquarium.items << twengal
