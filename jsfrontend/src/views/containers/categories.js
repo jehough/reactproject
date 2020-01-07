36f117195cc8a43e334f16a1aa782ba30a1cdc1b
jehough <teachlife@outlook.com>
2020-01-07 10:49:25 -0500

    edits category container

--- jsfrontend/src/views/containers/categories.js
+++ jsfrontend/src/views/containers/categories.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 
 export default class Home extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list: []
+    }
+  }
+
 
   render(){
     return(<div>
       <h1>Category: {this.props.title}</h1>
-
-    </div>)
+      <CategoryList list={this.state.list}/>
+      </div>)
   }
 }
