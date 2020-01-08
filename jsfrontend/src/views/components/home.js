dc9f962a433f4d8571843927666fcc90605e961e
jehough <teachlife@outlook.com>
2020-01-07 22:17:19 -0500

    adds carousel to the home page

--- jsfrontend/src/views/components/home.js
+++ jsfrontend/src/views/components/home.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import Carousel from 'react-bootstrap/Carousel'
 
 export default class Home extends Component {
 
   render(){
     return(<div>
       <h1>Welcome</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img className="carouselImage" src="./coral.jpeg" alt="First Slide"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img clasName="carouselImage" src="./blackskirt.jpeg" alt="Second Slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="carouselImage" src="./Koi.jpeg" alt="Third Slide" />
+        </Carousel.Item>
+      </Carousel>
     </div>)
   }
 }
