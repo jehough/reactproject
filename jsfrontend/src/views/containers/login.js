9d0268e7575dc8086bce5e9689c7b1f1a6ac41dd
jehough <teachlife@outlook.com>
2020-01-09 15:19:13 -0500

    adds login form component

--- jsfrontend/src/views/containers/login.js
+++ jsfrontend/src/views/containers/login.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import DisplayTitle from '../components/title'
+import LoginForm from '../components/login_form'
 
 export default class Login extends Component {
 
   render(){
     return(<div>
-
+      <DisplayTitle title="Login Page" />
+      <h4>You must login to view your cart.</h4>
+      <LoginForm />
     </div>)
   }
 }
